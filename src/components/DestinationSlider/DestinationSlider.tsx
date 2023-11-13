ae95a862578d98b8406ff171c5f3b6c374e150db
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-11-13 11:52:23 -0300

    Component created
    - slider that holds data from destination page

---
+++ src/components/DestinationSlider/DestinationSlider.tsx
@@ -0,0 +1,71 @@
+"use client"
+import styles from "./DestinationSlider.module.scss"
+import data from "@/data/data.json"
+import Image from "next/image"
+import { useState } from "react"
+
+export const DestinationSlider = () => {
+	interface dataText {
+		name: string
+		images: {
+			png: string
+			webp: string
+		}
+		description: string
+		distance: string
+		travel: string
+	}
+
+	const destinationsData: dataText[] = data.destinations
+	const [slide, setSlide] = useState(0)
+
+	const handleNav = (index: number) => {
+		setSlide(index)
+	}
+
+	return (
+		<>
+			<ul className={styles.nav} data-slide={slide}>
+				{destinationsData.map((nav, index) => (
+					<li key={index} onClick={() => handleNav(index)} className={index === slide ? styles.activeNav : ''}>
+						{nav.name}
+					</li>
+				))}
+			</ul>
+			
+				{destinationsData.map(
+					(destinations, index) =>
+						index === slide && (
+							<div className={styles.contentBox}>
+								<div className={styles.imageSide}>
+									<Image
+										src={destinations.images.webp}
+										alt={`${destinations.name} image`}
+										width={445}
+										height={445}
+									/>
+								</div>
+								<div className={styles.infoSide}>
+									<h2 className={styles.destinationTitle}>
+										{destinations.name}
+									</h2>
+									<p className={styles.mainText}>{destinations.description}</p>
+									<span></span>
+									<div className={styles.travelInfo}>
+										<div>
+											<h3>AVG. DISTANCE</h3>
+											<h4>{destinations.distance}</h4>
+										</div>
+										<div>
+											<h3>Est. travel time</h3>
+											<h4>{destinations.travel}</h4>
+										</div>
+									</div>
+								</div>
+							</div>
+						)
+				)}
+			
+		</>
+	)
+}
