8365397699d1ea3dc71fe5d06d36fdd9eb88efb4
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-11-14 17:04:45 -0300

    Page update
    - added main components and styles to the page

--- src/app/technology/page.tsx
+++ src/app/technology/page.tsx
@@ -1,12 +1,14 @@
+import styles from "@/styles/pageStyles/technology.module.scss"
+import { PageTitle } from "@/components/PageTitle/PageTitle"
+import {TechonologySlider} from "@/components/TechnologySlider/TechonologySlider"
+
 export default function Technology() {
-    return (
-      <main>
-        <p style={{color:"white"}}>
-          So, you want to travel to
-         aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-          Explore
-        </p>
-      </main>
-    )
-  }
-  
+	return (
+		<main className={styles.technologyPageContainer}>
+			<div className={styles.content}>
+				<PageTitle titleId="03" titleText="SPACE LAUNCH 101" />
+				<TechonologySlider />
+			</div>
+		</main>
+	)
+}
