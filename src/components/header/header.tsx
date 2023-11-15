8ef37228a04fa598a888df28f0f5e08e9fa098ec
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-11-15 15:55:16 -0300

    Component update
    - now shows active page in the menu
    - style import update
    - style for active page added
    - variables names updated
    - breakpoints on desktop updated

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,30 +1,40 @@
-import Image from "next/image"
-import logo from "../../../public/assets/shared/logo.svg"
-import styles from "./Header.module.scss"
+'use client'
+
+import Image from 'next/image'
+import logo from '../../../public/assets/shared/logo.svg'
+import styles from './Header.module.scss'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export const Header = () => {
-	const menuItemId = ["00", "01", "02", "03"]
-	const menuTitle = ["home", "destination", "crew", "technology"]
+	const menuItemId = ['00', '01', '02', '03']
+	const menuTitle = ['home', 'destination', 'crew', 'technology']
+
+	const pathname = usePathname()
 
 
 	return (
 		<header className={styles.header}>
-			<Link href="/"><Image src={logo} alt="company logo" /></Link>
-				<nav className={styles.navMenu}>
-                <span></span>
-					<ul>
-						{menuItemId.map((id, index) => {                                                   
-							return (
-                                <Link key={index}href={`/${menuTitle[index] === 'home' ? '' : menuTitle[index]}`}>
-								<li key={index}>
+			<Link href="/">
+				<Image src={logo} alt="company logo" />
+			</Link>
+			<nav className={styles.navMenu}>
+				<span></span>
+				<ul>
+					{menuItemId.map((id, index) => {
+						return (
+							<Link
+								key={index}
+								href={`/${menuTitle[index] === 'home' ? '' : menuTitle[index]}`}
+							>
+								<li className={(menuTitle[index] === 'home' ? '/' : `/${menuTitle[index]}`) === pathname ? styles.activeNav : ''}>
 									<span>{id}</span> {menuTitle[index]}
 								</li>
-                                </Link>
-							)
-						})}
-					</ul>
-				</nav>			
+							</Link>
+						)
+					})}
+				</ul>
+			</nav>
 		</header>
 	)
 }
