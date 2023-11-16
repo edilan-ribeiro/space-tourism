43f58ea01059cbdc8dc89643e8b62a8212b36509
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-11-16 08:45:28 -0300

    Component update
    - moved slider nav to inside info container for better alignment

--- src/components/DestinationSlider/DestinationSlider.tsx
+++ src/components/DestinationSlider/DestinationSlider.tsx
@@ -1,8 +1,8 @@
-"use client"
-import styles from "./DestinationSlider.module.scss"
-import data from "@/data/data.json"
-import Image from "next/image"
-import { useState } from "react"
+'use client'
+import styles from './DestinationSlider.module.scss'
+import data from '@/data/data.json'
+import Image from 'next/image'
+import { useState } from 'react'
 
 export const DestinationSlider = () => {
 	interface dataText {
@@ -25,47 +25,49 @@ export const DestinationSlider = () => {
 
 	return (
 		<>
-			<ul className={styles.nav} data-slide={slide}>
-				{destinationsData.map((nav, index) => (
-					<li key={index} onClick={() => handleNav(index)} className={index === slide ? styles.activeNav : ''}>
-						{nav.name}
-					</li>
-				))}
-			</ul>
-			
-				{destinationsData.map(
-					(destinations, index) =>
-						index === slide && (
-							<div key={index} className={styles.contentBox}>
-								<div className={styles.imageSide}>
-									<Image
-										src={destinations.images.webp}
-										alt={`${destinations.name} image`}
-										width={445}
-										height={445}
-									/>
-								</div>
-								<div className={styles.infoSide}>
-									<h2 className={styles.destinationTitle}>
-										{destinations.name}
-									</h2>
-									<p className={styles.mainText}>{destinations.description}</p>
-									<span></span>
-									<div className={styles.travelInfo}>
-										<div>
-											<h3>AVG. DISTANCE</h3>
-											<h4>{destinations.distance}</h4>
-										</div>
-										<div>
-											<h3>Est. travel time</h3>
-											<h4>{destinations.travel}</h4>
-										</div>
+			{destinationsData.map(
+				(destinations, index) =>
+					index === slide && (
+						<div key={index} className={styles.contentBox}>
+							<div className={styles.imageSide}>
+								<Image
+									src={destinations.images.webp}
+									alt={`${destinations.name} image`}
+									width={445}
+									height={445}
+								/>
+							</div>
+							<div>
+								<ul className={styles.nav} data-slide={slide}>
+									{destinationsData.map((nav, index) => (
+										<li
+											key={index}
+											onClick={() => handleNav(index)}
+											className={index === slide ? styles.activeNav : ''}
+										>
+											{nav.name}
+										</li>
+									))}
+								</ul>
+								<div  className={styles.infoSide}>
+								<h2 className={styles.destinationTitle}>{destinations.name}</h2>
+								<p className={styles.mainText}>{destinations.description}</p>
+								<span></span>
+								<div className={styles.travelInfo}>
+									<div>
+										<h3>AVG. DISTANCE</h3>
+										<h4>{destinations.distance}</h4>
+									</div>
+									<div>
+										<h3>Est. travel time</h3>
+										<h4>{destinations.travel}</h4>
+									</div>
 									</div>
 								</div>
 							</div>
-						)
-				)}
-			
+						</div>
+					)
+			)}
 		</>
 	)
 }
